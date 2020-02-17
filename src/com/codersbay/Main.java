96279063b6aea1dad9638d55b9e3a6193f2061d7
Mario Wagner <ma_wa@gmx.at>
2020-02-17 15:11:05 +0100

    Created first logic

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,4 +1,21 @@
 package com.codersbay;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] unsorted = {5, 4, 1, 9, 6, 7, 3, 2, 8};
+        int countRight = 1;
+        int countLeft = 1;
+        int temp;
+
+        for (int i = 1; i <= unsorted.length; i++) {
+            if (unsorted[i] < unsorted[i - 1]) {
+                temp = unsorted[i];
+                unsorted[i] = unsorted[i - 1];
+                unsorted[i - countLeft] = temp;
+            }
+        }
+        for (int p : unsorted) {
+            System.out.print(p + " ");
+        }
+    }
 }
