4bc9990224682c5fa3bbbdc98a7a30118f99872d
Mario Wagner <ma_wa@gmx.at>
2020-02-17 15:23:22 +0100

    Optimized Algorithm

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -3,15 +3,15 @@ package com.codersbay;
 public class Main {
     public static void main(String[] args) {
         int[] unsorted = {5, 4, 1, 9, 6, 7, 3, 2, 8};
-        int countRight = 1;
-        int countLeft = 1;
         int temp;
-
-        for (int i = 1; i <= unsorted.length; i++) {
-            if (unsorted[i] < unsorted[i - 1]) {
-                temp = unsorted[i];
-                unsorted[i] = unsorted[i - 1];
-                unsorted[i - countLeft] = temp;
+        for (int j = 0; j < unsorted.length; j++) {
+            for (int i = 1; i < unsorted.length; i++) {
+                if (unsorted[i] < unsorted[i - 1]) {
+                    temp = unsorted[i];
+                    unsorted[i] = unsorted[i - 1];
+                    unsorted[i - 1] = temp;
+                    // System.out.println(temp);
+                }
             }
         }
         for (int p : unsorted) {
