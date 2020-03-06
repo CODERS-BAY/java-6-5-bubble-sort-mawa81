75ddc32ef9aeed1569cd0198118c73664fe33bbc
Mario Wagner <ma_wa@gmx.at>
2020-03-06 10:02:48 +0100

    Created methods

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,21 +1,30 @@
 package com.codersbay;
 
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
         int[] unsorted = {5, 4, 1, 9, 6, 7, 3, 2, 8};
-        int temp;
+
+        int[] sorted = bubbleSort(unsorted);
+        System.out.println(Arrays.toString(sorted));
+    }
+
+    private static int[] bubbleSort(int[] unsorted) {
         for (int j = 0; j < unsorted.length; j++) {
-            for (int i = 1; i < unsorted.length; i++) {
-                if (unsorted[i] < unsorted[i - 1]) {
-                    temp = unsorted[i];
-                    unsorted[i] = unsorted[i - 1];
-                    unsorted[i - 1] = temp;
-                    // System.out.println(temp);
-                }
-            }
+            sortTwo(unsorted);
         }
-        for (int p : unsorted) {
-            System.out.print(p + " ");
+        return unsorted;
+    }
+
+    private static void sortTwo(int[] unsorted) {
+        for (int i = 1; i < unsorted.length; i++) {
+            if (unsorted[i] < unsorted[i - 1]) {
+                int temp = unsorted[i];
+                unsorted[i] = unsorted[i - 1];
+                unsorted[i - 1] = temp;
+            }
         }
     }
 }
